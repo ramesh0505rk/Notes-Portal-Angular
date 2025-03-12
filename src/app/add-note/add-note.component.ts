7f226645815ff5fab5b94a6835526cdd4db9d042
RAMESHKUMAR <t476797@deluxe.com>
2025-03-12 20:32:37 +0530

    Add note method - Initial

--- src/app/add-note/add-note.component.ts
+++ src/app/add-note/add-note.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NotesService } from '../Services/notes.service';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import OktaAuth from '@okta/okta-auth-js';
 
 @Component({
   selector: 'app-add-note',
@@ -9,23 +12,41 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
   templateUrl: './add-note.component.html',
   styleUrl: './add-note.component.scss'
 })
-export class AddNoteComponent {
+export class AddNoteComponent implements OnInit {
 
   noteForm: FormGroup
+  isAuthenticated: Boolean = false
+  user: any
+  userId: string = ''
 
   @Output() closeAddNote = new EventEmitter<Boolean>()
 
-  constructor() {
+  constructor(@Inject(OKTA_AUTH) private readonly oktaAuth: OktaAuth, private notesService: NotesService) {
     this.noteForm = new FormGroup({
       title: new FormControl('', [Validators.required]),
       content: new FormControl('', [Validators.required])
     })
+  }
+  async ngOnInit() {
+    this.isAuthenticated = await this.oktaAuth.isAuthenticated()
+    if (this.isAuthenticated) {
+      this.user = await this.oktaAuth.getUser()
+      this.userId = this.user.sub
+    }
   }
 
   onClose() {
     this.closeAddNote.emit(true)
   }
   onSaveNote() {
-
+    const { title, content } = this.noteForm.value
+    try {
+      this.notesService.addNote(this.userId,title,content).subscribe(result=>{
+        
+      })
+    }
+    catch (e) {
+      console.error(e)
+    }
   }
 }
